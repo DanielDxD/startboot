5435c706a98754406e6aed5b49c2f6c5fbf1acae
Daniel Lopes <daniel@cristalstudio.net>
2023-02-14 02:45:47 -0300

    Add swagger

--- src/main/java/com/daniellopes/startboot/controller/ClientController.java
+++ src/main/java/com/daniellopes/startboot/controller/ClientController.java
@@ -2,26 +2,31 @@ package com.daniellopes.startboot.controller;
 
 import com.daniellopes.startboot.model.Client;
 import com.daniellopes.startboot.repository.ClientRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Api(value = "Clients")
 @RequestMapping("/clients")
 public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
 
+
+    @ApiOperation(value = "List all clients")
     @GetMapping
     public List<Client> list() {
         return clientRepository.findAll();
     }
+    @ApiOperation(value = "Get a client")
     @GetMapping("/{id}")
     public Client get(@PathVariable Long id) {
         Optional<Client> currentClient = clientRepository.findById(id);
@@ -32,12 +37,14 @@ public class ClientController {
         return currentClient.get();
     }
 
+    @ApiOperation(value = "Create a client")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Client create(@RequestBody Client client) {
         return clientRepository.save(client);
     }
 
+    @ApiOperation(value = "Update a client")
     @PutMapping
     public Client edit(@RequestBody Client client) {
         Optional<Client> currentClient = clientRepository.findById(client.id);
@@ -51,6 +58,7 @@ public class ClientController {
         return editedClient;
     }
 
+    @ApiOperation(value = "Delete a client")
     @DeleteMapping("/{id}")
     public void remove(@PathVariable Long id) {
         Optional<Client> currentClient = clientRepository.findById(id);
